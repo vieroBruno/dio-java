f822ee719603552dd923b405ae76d312a52f54ce
Bruno <viero.g.bruno@gmail.com>
2024-05-22 22:31:12 -0300

    Aulas 22-5

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.print("Sei lá irmão");
+        int ano = 2024;
+        ano = 2025;
+        final String BR = "Brasil";
+          
+    }
+}
